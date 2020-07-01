4bfb54ec517be7a47e1b67f72ad0e1f47bb743cd
1712480 <danghungvn2602@gmail.com>
2020-07-01 16:53:23 +0700

    setup postgres

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const pool = require('./src/pg')
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({greeting: "This is data from server"});
+    pool.query('SELECT * FROM Users', (err, res) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            console.log(res.rows[0]);
+        }
+    
+    })
 })
+
+module.exports = (a, b) => {
+    return a + b;
+}
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
---
+++ index.test.js
@@ -0,0 +1,5 @@
+const sum = require('./');
+
+test('test 1 + 1', () => {
+    expect(sum(1, 1)).toBe(2);
+});
